5dd5d21a8d2064c00d6a67f18ffc652014e79447
TAY TS <taytzushieh@gmail.com>
2019-02-07 12:40:36 +0800

    chore: Clean up the configuration files

--- src/main.js
+++ src/main.js
@@ -1,24 +1,11 @@
 import Vue from "vue";
-import './plugins/vuetify'
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
-import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
-Vue.use(Vuetify, {
-  basePath: "./assets",
-  theme: {
-    primary: "#d099ef",
-    secondary: "#ffffff",
-    accent: "9f2edf",
-    background: "a991b6"
-  },
-  iconfont: "md"
-});
 
 new Vue({
   router,
