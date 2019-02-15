92468bda646c72358a528698605462a3f2b3b440
Kenneth Sim <kensim28@hotmail.com>
2019-02-15 23:01:23 +0800

    feature: Added navigation to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer permanent>
-      <v-toolbar height="400%">
-        <v-list>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      permanent
+      absolute
+      width="175"
+    >
+      <v-toolbar flat class="py-3" color = "rgba(247,232,255,100)">
           <v-list-tile>
-            <v-list-tile-title class="title">
-            </v-list-tile-title>
+              <img class="logo-main" src="../assets/img/Accenture_Support_Staff.svg" alt >
           </v-list-tile>
-        </v-list>
       </v-toolbar>
-  
-      <v-divider></v-divider>
-  
-      <v-list dense class="pt-0">
+      <v-toolbar flat class="py-1" color = "rgba(222,222,222,100)">
+        <b>{{ adminName }}</b>
+      </v-toolbar>
+
+      <v-list class="pt-0" dense>
+
         <v-list-tile
           v-for="item in items"
           :key="item.title"
@@ -21,36 +25,44 @@
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-  
+
           <v-list-tile-content>
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-  </v-app>
+
 </template>
 
 <script>
-export default {
-    data: () => {
-            return {
+  export default {
+    data () {
+      return {
+        adminName: "Insert admin name here",
+        drawer: true,
         items: [
-            { title: 'Home', icon: 'dashboard' },
-            { title: 'About', icon: 'question_answer' }
+          //fix icons sizing
+          { title: 'New Jobs', icon: '../assets/img/_ionicons_svg_ios-document.svg' },
+          { title: 'My Jobs', icon: '../assets/img/_ionicons_svg_md-briefcase.svg' }
         ],
         right: null
-        }
+
+        
+      }
     }
-}
+  }
 </script>
 
 <style scoped>
+.logo-area{
+  color: #f7e8ff
+}
+
 .logo-main {
-  width: 40vw;
+  width: 150px;
   -webkit-transition: all 0.5s;
   transition: all 0.5s;
-  transform: translateX(-20%);
-  opacity: 0;
+  transform: translate(-14%,5%);
 }
 </style>
