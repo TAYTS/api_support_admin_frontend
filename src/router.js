a8db81f5fc1cdc902566873f74db517039b362a9
TAY TS <taytzushieh@gmail.com>
2019-02-19 00:47:12 +0800

    Revert "Feature/dashboard"

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Dashboard from "./views/Dashboard.vue";
+import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -10,17 +10,17 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "LoginPage",
-      component: Dashboard
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // }
   ]
 });
