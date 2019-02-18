22e407188a532dc521abc9988e256c6682ae01da
Kenneth Sim <kensim28@hotmail.com>
2019-02-18 22:30:25 +0800

    fix: Fixed nav bar picture and button layout issue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,60 +1,103 @@
 <template>
-  <v-navigation-drawer v-model="drawer" permanent absolute width="175">
-    <v-toolbar flat class="py-3" color="rgba(247,232,255,100)">
-      <v-list-tile>
-        <img class="logo-main" src="../assets/img/Accenture_Support_Staff.svg" alt>
-      </v-list-tile>
-    </v-toolbar>
-    <v-toolbar flat class="py-1" color="rgba(222,222,222,100)">
+  <v-navigation-drawer v-model="drawer" fixed permanent absolute width="175">
+    <div id="logo-bg">
+      <div id="logo-main">
+        <img src="../assets/img/Accenture_Support_Staff.svg" alt>
+      </div>
+    </div>
+    <div id="name-bg">
+      <div id="name-main">
       <b>{{ adminName }}</b>
-    </v-toolbar>
-
-    <v-list class="pt-0" dense>
-      <v-list-tile v-for="item in items" :key="item.title" @click>
-        <v-list-tile-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-tile-action>
-
-        <v-list-tile-content>
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
+      </div>
+    </div>
+    <div>
+      <button class="button">
+       <img class="button-image" src="../assets/img/_ionicons_svg_ios-document.svg" alt>
+          <div class="button-text">
+           New Jobs
+           </div>
+      </button>
+      <button class="button">
+       <img class="button-image" src="../assets/img/_ionicons_svg_md-briefcase.svg" alt>
+          <div class="button-text">
+           My Jobs
+           </div>
+      </button>
+    </div>
   </v-navigation-drawer>
 </template>
+
+<style scoped>
+#logo-bg {
+  background-color: #f7e8ff;
+  padding-top: 10px;
+}
+
+#logo-main {
+  width: 130px;
+  margin: auto;
+  padding-right:4px;
+}
+
+#name-bg {
+  background-color: #dedede;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  
+}
+
+#name-main {
+  text-align: center;
+}
+
+.button-image{
+  width: 30px;
+  margin-top:7px;
+  float: left;
+  margin-left:30px;
+}
+
+.button-text{
+  text-align: left;
+  padding:10px;
+  margin-left: 60px;
+  font-size: 18px;
+  color: #000000;
+}
+
+.button {
+  background-color: #FFFFFF;
+  display: block;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  text-decoration: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background: #f0f0f0;
+}
+
+.button:active {
+    background: #ffffff;
+    transition-duration: 0.1s;
+}
+
+.button:focus {
+  outline:0;
+  background: #f0ddf5;
+  transition-duration: 0.1s;
+  }
+
+</style>
 
 <script>
 export default {
   data() {
     return {
       adminName: "Insert admin name here",
-      drawer: true,
-      items: [
-        //fix icons sizing
-        {
-          title: "New Jobs",
-          icon: "../assets/img/_ionicons_svg_ios-document.svg"
-        },
-        {
-          title: "My Jobs",
-          icon: "../assets/img/_ionicons_svg_md-briefcase.svg"
-        }
-      ],
-      right: null
     };
   }
 };
 </script>
-
-<style scoped>
-.logo-area {
-  color: #f7e8ff;
-}
-
-.logo-main {
-  width: 150px;
-  -webkit-transition: all 0.5s;
-  transition: all 0.5s;
-  transform: translate(-14%, 5%);
-}
-</style>
