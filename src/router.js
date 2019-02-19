985829f14db13623c547ecb8c5b6f59ff4d2e9ba
TAY TS <taytzushieh@gmail.com>
2019-02-19 23:17:30 +0800

    feature: Update vue-router to cater the login and dummy user page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import LoginPage from "./views/LoginPage.vue";
+import User from "./views/User.vue";
 
 Vue.use(Router);
 
@@ -9,18 +10,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
+      path: "/login",
       name: "LoginPage",
       component: LoginPage
+    },
+    {
+      path: "/",
+      name: "UserPage",
+      component: User
     }
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // }
   ]
 });
