94bb66d66166ac5ea19d2e0b18585e321396e1a9
Kenneth Sim <kensim28@hotmail.com>
2019-03-23 21:04:50 +0800

    Feature: added Today/yesterday/oldermsg headers in messagelist

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -31,9 +31,11 @@
     </div>
     <v-list three-line class="items">
       <template v-for="(item, index) in filteredItems">
-        <v-subheader v-if="item.header" :key="item.header">{{
+        <v-subheader v-if="item.header" :key="item.header">
+          {{
           item.header
-        }}</v-subheader>
+          }}
+        </v-subheader>
         <div v-else-if="item.divider" :key="index">
           <v-divider></v-divider>
           <v-list-tile v-if="item.title" :key="index" @click>
@@ -41,9 +43,7 @@
               <v-layout fill-height class="pa-0">
                 <v-list-tile-content>
                   <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                  <v-list-tile-sub-title
-                    v-html="item.subtitle"
-                  ></v-list-tile-sub-title>
+                  <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-layout>
             </v-container>
@@ -96,7 +96,48 @@ export default {
       if (response !== 0) {
         console.log(response.length);
         var i;
+        var headerCheck = {
+          Today: 0,
+          Yesterday: 0,
+          Older: 0
+        };
+        var today = new Date();
+        var yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
         for (i = 0; i < response.length; i++) {
+          var postDate = new Date(response[i].dateTime);
+          if (
+            headerCheck["Today"] == 0 &&
+            postDate.getDate() == today.getDate() &&
+            postDate.getMonth() == today.getMonth() &&
+            postDate.getFullYear() == today.getFullYear()
+          ) {
+            headerCheck["Today"] = 1;
+            this.items.push({
+              header: "Today"
+            });
+          }
+          if (
+            headerCheck["Yesterday"] == 0 &&
+            postDate.getDate() == yesterday.getDate() &&
+            postDate.getMonth() == yesterday.getMonth() &&
+            postDate.getFullYear() == yesterday.getFullYear()
+          ) {
+            headerCheck["Yesterday"] = 1;
+            this.items.push({
+              header: "Yesterday"
+            });
+          }
+          if (
+            headerCheck["Older"] == 0 &&
+            postDate.getDate() < yesterday.getDate() ||
+            postDate.getMonth() < yesterday.getMonth() ||
+            postDate.getFullYear() < yesterday.getFullYear()
+          ) {
+            headerCheck["Older"] = 1;
+            this.items.push({
+              header: "Older Tickets"
+            });
+          }
           this.items.push({
             title: response[i].title,
             subtitle: response[i].body,
