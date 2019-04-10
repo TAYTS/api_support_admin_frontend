b78275809e369c6400c0126001134b0edae9af08
TAY TS <taytzushieh@gmail.com>
2019-04-10 13:27:47 +0800

    chore: Move style to the end of file

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -31,9 +31,7 @@
     </div>
     <v-list three-line class="items">
       <template v-for="(item, index) in filteredItems">
-        <v-subheader v-if="item.header" :key="item.header">
-          {{ item.header }}
-        </v-subheader>
+        <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
         <div v-else-if="item.divider" :key="index">
           <v-divider></v-divider>
           <v-list-tile
@@ -45,14 +43,8 @@
             <v-container class="pa-0 ma-0">
               <v-layout fill-height class="pa-0">
                 <v-list-tile-content>
-                  <v-list-tile-title
-                    class="title"
-                    v-html="item.title"
-                  ></v-list-tile-title>
-                  <v-list-tile-sub-title
-                    class="subtitle"
-                    v-html="item.subtitle"
-                  ></v-list-tile-sub-title>
+                  <v-list-tile-title class="title" v-html="item.title"></v-list-tile-title>
+                  <v-list-tile-sub-title class="subtitle" v-html="item.subtitle"></v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-layout>
             </v-container>
@@ -62,6 +54,40 @@
     </v-list>
   </v-navigation-drawer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: "",
+      adminName: "Insert admin name here",
+      filter: ["Foo", "Bar", "Fizz", "Buzz"],
+      items: [],
+      selectedClass: "selected-class",
+      lastIndex: 0
+    };
+  },
+  mounted() {},
+  computed: {
+    filteredItems: function() {
+      return this.$parent.items.filter(item => {
+        if (this.search.length > 0) {
+          if (item.title) {
+            return item.title.toLowerCase().match(this.search.toLowerCase());
+          }
+        } else {
+          return true;
+        }
+      });
+    }
+  },
+  methods: {
+    openMessage: function(index, postID) {
+      this.$parent.openMessage(index, postID);
+    }
+  }
+};
+</script>
 
 <style scoped>
 /* width */
@@ -99,37 +125,3 @@
   background-color: rgb(245, 245, 245);
 }
 </style>
-
-<script>
-export default {
-  data() {
-    return {
-      search: "",
-      adminName: "Insert admin name here",
-      filter: ["Foo", "Bar", "Fizz", "Buzz"],
-      items: [],
-      selectedClass: "selected-class",
-      lastIndex: 0
-    };
-  },
-  mounted() {},
-  computed: {
-    filteredItems: function() {
-      return this.$parent.items.filter(item => {
-        if (this.search.length > 0) {
-          if (item.title) {
-            return item.title.toLowerCase().match(this.search.toLowerCase());
-          }
-        } else {
-          return true;
-        }
-      });
-    }
-  },
-  methods: {
-    openMessage: function(index, postID) {
-      this.$parent.openMessage(index, postID);
-    }
-  }
-};
-</script>
