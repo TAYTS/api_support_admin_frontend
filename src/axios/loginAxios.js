cdbaab9bf5580ab2e68db5f400f5839c1e8ed07f
TAY TS <taytzushieh@gmail.com>
2019-04-24 09:13:29 +0800

    feature: Update axios baseURL

--- src/axios/loginAxios.js
+++ src/axios/loginAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const loginAxios = axios.create({
-  baseURL: "http://127.0.0.1:5002",
+  baseURL: "https://authenticate.chocolatepie.tech",
   headers: {
     "Content-Type": "application/json"
   },
